77c9b83dab487d2b23a142f1edd3ae6b6f25c323
monkey1992 <yuriyshea@163.com>
2018-02-10 16:55:46 +0800

    Update some annotations

--- app/src/test/java/com/xy/kotlin/FilterOperatorTest.kt
+++ app/src/test/java/com/xy/kotlin/FilterOperatorTest.kt
@@ -5,9 +5,7 @@ import org.junit.Test
 import org.junit.Assert.*
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see [Testing documentation](http://d.android.com/tools/testing)
+ * Unit test for filter operators.
  */
 class FilterOperatorTest {
 
